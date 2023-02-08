eb7658497c09dda93c4744aff287266fff8b6c6b
Thitiwudh Uasmith <tdy2012@Thitiwudhs-MacBook-Pro.local>
2023-02-08 15:54:17 +0700

    feat: Added answer for exercise 5.

---
+++ no_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static long Fact(long n)
+    {
+        if(n <= 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Fact(n-1);
+        }
+    }
+
+    static long CombinationCount(long n, long r)
+    {
+        return Fact(n)/(Fact(n-r)*Fact(r));
+    }
+
+    static double Binomial(long n, long x, double p)
+    {
+        return CombinationCount(n, x)*Math.Pow(p, x)*Math.Pow(1-p, n-x);
+    }
+
+    static void Main(string[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+        long x = long.Parse(Console.ReadLine());
+        double p = double.Parse(Console.ReadLine());
+        Console.WriteLine(Binomial(n, x, p));
+    }
+}
