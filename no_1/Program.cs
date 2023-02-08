8534e4536c070af4e62e22f3c9f17314f4cca888
Thitiwudh Uasmith <tdy2012@Thitiwudhs-MacBook-Pro.local>
2023-02-08 14:48:05 +0700

    feat: Added answer for exercise 1.

---
+++ no_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static long Fact(long n)
+    {
+        if(n <= 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Fact(n-1);
+        }
+    }
+
+    static long PermutationCount(long n, long r)
+    {
+        return Fact(n)/Fact(n-r);
+    }
+
+    static void Main(string[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+        long r = long.Parse(Console.ReadLine());
+        Console.WriteLine(PermutationCount(n, r));
+    }
+}
